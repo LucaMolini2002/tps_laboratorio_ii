d3055b4096223f8435d5ddf648ad90b1e71c578c
LucaMolini2002 <luca.molinist@gmail.com>
2022-05-02 03:16:56 -0300

    tp subido

---
+++ TP2/TP-02/TP-02/Entidades/Sedan.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades
+{
+    public class Sedan : Vehiculo
+    {
+        //Atributos
+        ETipo tipo;
+        /// <summary>
+        /// Tipos de Sedan
+        /// </summary>
+        public enum ETipo
+        {
+            CuatroPuertas,
+            CincoPuertas
+        }
+        
+        //Contrsuctores
+
+        /// <summary>
+        /// Por defecto, TIPO será CuatroPuertas
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color) : base(chasis, marca, color)
+        {
+            tipo = ETipo.CuatroPuertas;
+        }
+
+        public Sedan(EMarca marca, string chasis, ConsoleColor color, ETipo tipo)
+           : this(marca, chasis, color)
+        {
+            this.tipo = tipo;
+        }
+
+        //Propiedades
+        /// <summary>
+        /// Sedan son 'Mediano'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Mediano;
+            }
+        }
+
+        //Metodos
+        /// <summary>
+        /// Muestra la informacion de un Sedan en un string
+        /// </summary>
+        /// <returns></returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SEDAN");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("TAMAÑO : {0} TIPO : {1}", this.Tamanio, this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TP2/TP-02/TP-02/Entidades/Suv.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Suv : Vehiculo
+    {
+        //Constructores
+        public Suv(EMarca marca, string chasis, ConsoleColor color) : base(chasis, marca, color) { }
+
+        //Propiedades
+
+        /// <summary>
+        /// SUV son 'Grande'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Grande;
+            }
+        }
+
+        //metodos
+       
+        /// <summary>
+        /// Muestra la informacion de un SUV en un string
+        /// </summary>
+        /// <returns></returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SUV");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine($"TAMAÑO: {this.Tamanio}");
+            sb.AppendLine("");
+            sb.AppendLine("--------------------");
+
+            return sb.ToString();
+        }
+
+    }
+}
